f688231071ba44a628609f309e14c7ff979139d2
Alexander Ng <alexng353@gmail.com>
2022-10-06 09:57:31 -0700

    change set role

--- pages/api/setrole.ts
+++ pages/api/setrole.ts
@@ -11,7 +11,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         email: 'alexng353@gmail.com',
       },
       data: {
-        admin: true,
+        writer: true,
       },
     })
     .then(() => {
